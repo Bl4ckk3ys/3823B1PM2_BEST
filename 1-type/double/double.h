60e64a09c22eedf42b9de883afa497319375180f
Bl4ckk3ys <ilchechnev@gmail.com>
2023-10-05 17:51:58 +0300

    New commit

--- 1-type/double/double.h
+++ 1-type/double/double.h
@@ -1,9 +1,13 @@
 bool compare_double(double x, double y) {
-    return true; // Not implementation
+    
+    if (fabs(x - y)<0.00000001) return true;
+    else return false;
 }
 
 int get_nearest_int(double x) {
-    return 0; // Not implementation
+    #include <math.h>
+    return x-=floor(x);
+    
 }
 
 double get_fractional(double x) {
