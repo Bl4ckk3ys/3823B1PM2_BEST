af87ed42cee93c4c5f5a304eb5778a2d39c39c2a
Bl4ckk3ys <ilchechnev@gmail.com>
2023-10-19 18:00:09 +0300

    New commit

--- 1-type/char-category/char-category.h
+++ 1-type/char-category/char-category.h
@@ -2,15 +2,21 @@
 
 
 bool is_digit(char x) {
-    return true; // No implementation
+        int tmp = (int)x; 
+        if (tmp >= 48 && tmp<=57) return true; 
+        
 }
 
 bool is_letter(char x) {
-    return true; // No implementation
+    int tmp = (int)x; 
+    if ((tmp >= 65 && tmp<=90)||(tmp >= 97 && tmp<= 122)) return true; 
+    
 }
 
 bool is_punctuation(char x) {
-    return true; // No implementation
+    int tmp = (int)x; 
+    if ((tmp >= 32 && tmp<=47)||(tmp >= 58 && tmp<= 64)||(tmp >= 91 && tmp<= 96)||(tmp >= 123 && tmp<= 126)) return true; 
+  
 }
 
 int get_ascii_code(char first, char second, char third) {
--- 2-if_else_switch/Change/change.h
+++ 2-if_else_switch/Change/change.h
@@ -1,3 +1,9 @@
 long long change(int a, int b, int c, int d){
-    return 0;
-}
+    long  price = a*100+b, payment = c*100+d;
+    if (price > payment) return -1;
+    else if (price == payment) return 0;
+    else if (price < payment) {
+        long tmp = payment - price;
+        return tmp;
+        }
+}   
