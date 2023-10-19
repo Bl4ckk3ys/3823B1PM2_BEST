fe4ce03a02920fd74c6071eeb25b3ea336531014
bl4ckk3ys <ilchechnev@gmail.com>
2023-10-19 23:17:17 +0300

    New commit

---
+++ build/1-type/CMakeFiles/module.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/module.dir/module/test.c.obj"
+  "libmodule.dll.a"
+  "module.exe"
+  "module.exe.manifest"
+  "module.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/module.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/1-type/CMakeFiles/seconds.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/seconds.dir/seconds/test.c.obj"
+  "libseconds.dll.a"
+  "seconds.exe"
+  "seconds.exe.manifest"
+  "seconds.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/seconds.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
