5bc36414d4cde06a2d950df73da184b2168bc0d9
bl4ckk3ys <ilchechnev@gmail.com>
2023-10-20 00:04:37 +0300

    New commit

--- 1-type/decomposition/decomposition.h
+++ 1-type/decomposition/decomposition.h
@@ -1,8 +1,14 @@
 int reverse(int number) {
-    return 0; // Not implementation
+    int ans = 0, tmp = number;
+    while (tmp>0){
+        ans = ans*10 + (tmp%10);
+        tmp /= 10;
+    }
+    return ans;
 }
 
 double decomposition(int number) {
+    
     return 0; // Not implementation
 }
 
