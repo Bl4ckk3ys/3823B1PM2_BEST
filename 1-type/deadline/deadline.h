cfeb6b084a7865827f915cc5df931166365ada5c
bl4ckk3ys <ilchechnev@gmail.com>
2023-10-23 00:12:24 +0300

    New commit

--- 1-type/deadline/deadline.h
+++ 1-type/deadline/deadline.h
@@ -1,3 +1,13 @@
 int get_score(short score, int start, int stop, int now) {
     
+    if (now <= start) return score;
+    else if(now>=stop){
+        if (score%2 == 1) return (int)(score/2)+1;
+        else return score/2;
+    }
+    else if((now > start) && (now < stop)){
+        double r=score*((double)(start-now+stop)/(stop));
+        if (r - (int)r>0) return (int)(r+1);
+        else return (int)r;
+    }
 }
--- 2-if_else_switch/Change/change.h
+++ 2-if_else_switch/Change/change.h
@@ -1,6 +1,5 @@
 long long change(int a, int b, int c, int d){
-    long long ch = ((c*100+d)-(a*100+b));
-    if (ch<0) return -1;
-    else if (ch==0) return 0;
-    else if (ch>0) return ch;
+    long long int ch = (((long long int)c*100+d)-((long long int)a*100+b));
+    if (ch>=0) return ch;
+    else return -1;
 }
