4e736b1e3cdb4b8caa0f9090d8dcb42f92489768
bl4ckk3ys <ilchechnev@gmail.com>
2023-10-24 20:54:01 +0300

    New commit

--- 2-if_else_switch/Chess/chess.h
+++ 2-if_else_switch/Chess/chess.h
@@ -1,7 +1,14 @@
+#include <stdbool.h>
 int chess_rook(char a, int b, char c, int d){
-    if((b>=1 && b<=8) && (d>=1 && d<=8)){
-        if (a==c) return 1;
-        else if (b==d)return 1;
+    char arr[] = {'A','B','C','D','E','F','G','H'};
+    bool flagA = false, flagC = false;
+    for(int i = 0;i<(sizeof(arr)/sizeof(char));i++){
+        if (a == arr[i]) flagA=true;
+        if (c == arr[i]) flagC=true;
+    }
+    if((b>=1 && b<=8) && (d>=1 && d<=8) && flagA && flagC){
+        if (a==c && b==d) return 0;
+        else if (a==c || b==d) return 1;
         else return 0;
         }
     return 0;
