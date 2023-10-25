ccf70d577dc63a8251de1ce8e7198f5075cdb1c9
bl4ckk3ys <ilchechnev@gmail.com>
2023-10-25 13:31:57 +0300

    New commit

--- 1-type/clock/clock.h
+++ 1-type/clock/clock.h
@@ -1,3 +1,7 @@
 int get_time(long long angle) {
-    return angle%360; 
+    int ans = 0;
+    ans += (int)((double)angle/8640)*10000;
+    ans += (int)((double)angle/360)*100;
+    ans += angle%360/6;
+    return ans; 
 }
