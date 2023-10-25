1beda519c88af82f577dd8814f4e58ea7ba55db9
bl4ckk3ys <ilchechnev@gmail.com>
2023-10-25 23:39:40 +0300

    New commit

--- 2-if_else_switch/MonthDay/monthday.h
+++ 2-if_else_switch/MonthDay/monthday.h
@@ -1,5 +1,22 @@
 int monthday(int month, int year){
     int leap = 0;
     if (year%400==0 || year%4==0) leap = 1;
-    if (leap == 1);
+    switch(month){
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12: return 31; break;
+        case 4: 
+        case 6:
+        case 9:
+        case 11:return 30; break;
+        case 2: switch (leap){
+            case 1: return 29;break;
+            default: return 28;break;
+        }
+    }
+
 }
