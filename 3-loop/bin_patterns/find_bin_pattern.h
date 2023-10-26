c77480dfb0dd7a275d4e66a1359568ae2186a0be
Bl4ckk3ys <ilchechnev@gmail.com>
2023-10-26 17:55:24 +0300

    New commit

--- 3-loop/bin_patterns/find_bin_pattern.h
+++ 3-loop/bin_patterns/find_bin_pattern.h
@@ -1,10 +1,19 @@
 #include <math.h>
 
 char find_bin_pattern(int number){
-    int x=0,i=0,c = 0;
+    int i=0,c = 0;
+    long long int x=0;
     while (number>0){
-        x = number%2;
+        x += number%2*pow(10,i);
         number/=2;
+        i++;
     }
+    while (x>0){
+        if (x%1000 == 101){
+            c+=1;
+            x/=100;
+        }
+        else x/=10;
+    }   
     return c;
 }
