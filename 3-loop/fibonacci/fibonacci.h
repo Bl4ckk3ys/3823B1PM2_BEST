62c3547cd950b09614e3d4c88702dc7b546500ae
Bl4ckk3ys <ilchechnev@gmail.com>
2023-11-02 16:51:06 +0300

    dc

--- 3-loop/fibonacci/fibonacci.h
+++ 3-loop/fibonacci/fibonacci.h
@@ -1,3 +1,9 @@
 long fib(int m){
-    return 0;
+    int n = 1, s = 0,tmp = 1;
+    while(n<m){
+        s += n;
+        n += tmp; 
+        tmp = n;
+    }
+    return s;
 }
--- 3-loop/fibonacci/test.c
+++ 3-loop/fibonacci/test.c
@@ -3,14 +3,6 @@
 
 
 void test_fib() {
-<<<<<<< HEAD
-    TEST_ASSERT(fib(5) == 15);
-    TEST_ASSERT(fib(100) == 505000);
-    TEST_ASSERT(fib(1000) == 3590035900000);
-    TEST_ASSERT(fib(73) == -5000050000);
-    TEST_ASSERT(fib(17) == 0);
-    TEST_ASSERT(fib(-17) == -1);
-=======
     TEST_ASSERT(fib(5) == 12);
     TEST_ASSERT(fib(100) == 232);
     TEST_ASSERT(fib(1000) == 2583);
@@ -18,7 +10,6 @@ void test_fib() {
     TEST_ASSERT(fib(17) == 33);
     TEST_ASSERT(fib(-17) == -1);
     TEST_ASSERT(fib(88005553) == 165580140);
->>>>>>> d162547550e79c7e156053a44ce9e25d95fbd359
 }
 
 TEST_LIST= {
