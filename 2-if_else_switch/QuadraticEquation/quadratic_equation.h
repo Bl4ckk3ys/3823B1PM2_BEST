d9cbf0b52c17b47beb4c025127387357b255faad
bl4ckk3ys <ilchechnev@gmail.com>
2023-11-02 23:44:23 +0300

    123

--- 2-if_else_switch/QuadraticEquation/quadratic_equation.h
+++ 2-if_else_switch/QuadraticEquation/quadratic_equation.h
@@ -1,3 +1,8 @@
+#include <math.h>
 int QuadraticEquation(double a, double b, double c){
-    return 0;
+    double x1,x2,d;
+    d = fabs(pow(b,2)-4*a*c);
+    x1 = ((-1)*b + sqrt(d))/(2*a);
+    x2 = ((-1)*b - sqrt(d))/(2*a);
+    return x1+x2;
 }
