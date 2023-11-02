25daa1ea8970c80930d6244c2b9af8fce95439a3
Bl4ckk3ys <ilchechnev@gmail.com>
2023-11-02 17:50:09 +0300

    inv

--- 3-loop/numinvers/numinvers.h
+++ 3-loop/numinvers/numinvers.h
@@ -1,3 +1,11 @@
+#include <math.h>
 int num_invers(int num){
-    return 0;
+    int ans = 0,i = 0;
+    while(num>0){
+        ans = ans*10+ num%10;
+        i++;
+        num/=10;
+    }
+    if (ans == 0) return 0;
+    else return ans;
 }
