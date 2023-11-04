3fcdf067e8b4914e177c35525c19dc4c06ba111e
bl4ckk3ys <ilchechnev@gmail.com>
2023-11-04 15:44:16 +0300

    row

--- 3-loop/row/calc_row.h
+++ 3-loop/row/calc_row.h
@@ -1,3 +1,17 @@
+#include <math.h>
 long long calc_row(int n, long long k){
-    return 0;
+    long long s = 0, tmp = 0;
+    int fl = 0;
+    for(int i = 1; i<n+1; i++){
+        tmp = s;
+        s+= i*k;
+        fl = 1;
+        if(fabs(s)<fabs(tmp)){
+            fl = 0;
+            return 0;
+            break;
+        }
+    }
+    if (fl == 1)return s;
+    else return 0;
 }
