ba6dfce526a6fe5e0b8e3b354e13f8212c713e8c
bl4ckk3ys <ilchechnev@gmail.com>
2023-11-06 17:59:25 +0300

    123

--- 3-loop/calc_e/calc_e.h
+++ 3-loop/calc_e/calc_e.h
@@ -1,5 +1,10 @@
 #include <math.h>
 
 unsigned long long calc_e(char order){
-    return 0;
+    double ans = 1;
+    int tmp = 1;
+    for(int i = 1;i<order;i++){
+        ans+= 1/i;
+    }
+    return ans;
 }
