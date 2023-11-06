f7d3cea9d46a3ab0057df0b4bcdafef650baec1a
bl4ckk3ys <ilchechnev@gmail.com>
2023-11-06 18:56:26 +0300

    intergral

--- 3-loop/integral/integral.h
+++ 3-loop/integral/integral.h
@@ -1,5 +1,7 @@
 #include <math.h>
 
 double integral(double a, double b, double c, double left, double right){
-    return 0.0;
+    double first = ((a*pow(right,3))/3 + (b*pow(right,2))/2 + c*right), second = ((a*pow(left,3))/3 + (b*pow(left,2))/2 + c*left);
+    if ((first - second)<0) return -1;
+    else return first - second;
 }
