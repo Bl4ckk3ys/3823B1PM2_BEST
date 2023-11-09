87c7dd71114d07dfeab35581e4b2262d522852b2
Bl4ckk3ys <ilchechnev@gmail.com>
2023-11-09 13:05:09 +0300

    123

--- 3-loop/find_digit/find_digit.h
+++ 3-loop/find_digit/find_digit.h
@@ -2,10 +2,7 @@
 
 char find_digit(double number, char digit){
     double tmp = number;
-    if (number == (int)number){
-        
-        while
-    }
+    
     //while(tmp>0);
     return 1;
 }
--- 4-array/get_percent/calc_percent.h
+++ 4-array/get_percent/calc_percent.h
@@ -3,8 +3,8 @@
 int calc_percent(int arr[], int size){
     int max=arr[0],min=arr[0],c = 0;
     for(int i = 0;i<size;i++){
-        if (max>arr[i]) max = arr[i];
-        if (min<arr[i]) min = arr[i];
+        if (max<arr[i]) max = arr[i];
+        if (min>arr[i]) min = arr[i];
     }
     double r = (max-min)/2;
     for(int i = 0;i<size;i++){
