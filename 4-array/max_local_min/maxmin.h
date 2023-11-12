d8b66d80d2e9161df890511ebcec4fb9a0c21561
bl4ckk3ys <ilchechnev@gmail.com>
2023-11-12 20:54:00 +0300

    maxmin

--- 4-array/max_local_min/maxmin.h
+++ 4-array/max_local_min/maxmin.h
@@ -5,6 +5,6 @@ int maxmin(int input[], int size){
     if (input[size-1]<input[size-2] && localMin<input[size-1]) localMin = input[size-1];
     for (int i=1;i<size-1;i++){
         if(input[i]<input[i-1] && input[i]<input[i+1] && input[i]>localMin) localMin = input[i];
-     }
+    }
     return localMin;
 }
