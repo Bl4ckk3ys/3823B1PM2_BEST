9fa51de1e8588e5397ae9e638b4151fea257ac26
bl4ckk3ys <ilchechnev@gmail.com>
2023-11-12 21:41:56 +0300

    hole

--- 4-array/find_hole/hole_finder.h
+++ 4-array/find_hole/hole_finder.h
@@ -1,5 +1,19 @@
 #include <math.h>
 
 int hole_finder(int matrix[], int length, int width){
-    return 0;
+    int minHole = pow(10,9), index;
+    for(int i = 4;i<length*width-4;i++){
+        if (matrix[i]<matrix[i-1] && matrix[i]<matrix[i-2] && matrix[i]<matrix[i-3] && matrix[i]<matrix[i-4] && matrix[i]<matrix[i+1] && matrix[i]<matrix[i+2] && matrix[i]<matrix[i+3] && matrix[i]<matrix[i+4] && matrix[i]<minHole){
+            minHole = matrix[i];
+            index = i;
+        } 
+    }
+    return ((int)((double)index/width))*100 + (index - (int)((double)index/width)*width)%width;
 }
+
+
+    // for(int i = 0;i<length;i++){
+    //    for(int j = 3;j<width;j++){
+    //         matrix[i*width+j];
+    //    }
+    // }
