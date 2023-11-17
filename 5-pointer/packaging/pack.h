b76e3c3636dfe23dfb6ee615803e7b76b42ad2f3
bl4ckk3ys <ilchechnev@gmail.com>
2023-11-17 11:46:48 +0300

    package

--- 5-pointer/packaging/pack.h
+++ 5-pointer/packaging/pack.h
@@ -1,11 +1,12 @@
 #include <math.h>
 
 unsigned long long pack_ull(unsigned char arr[], int size){
-    unsigned long long* pNum = &arr[0];
-    if (sizeof(unsigned long long) >= sizeof(arr[0]*size))return *pNum;
+    unsigned long long* pNum = arr;
+    if (sizeof(unsigned long long) >= sizeof(unsigned char)*size)return *pNum;
     return 0;
 }
 
 unsigned char unpack_ull(unsigned long long input){
-    return 5;
+    unsigned char* pInput = &input;
+    return *(pInput+4);
 }
