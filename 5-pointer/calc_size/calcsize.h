35d4d92c566e7d0e64418ffebfd4a72364d4ec08
bl4ckk3ys <ilchechnev@gmail.com>
2023-11-19 22:14:01 +0300

    123

--- 5-pointer/calc_size/calcsize.h
+++ 5-pointer/calc_size/calcsize.h
@@ -1,5 +1,33 @@
 #include <math.h>
 
 int calcsize(void* memory){
+    char* tmp = memory;
+    int bytes_between = 0, fl = 0, i = 0, ans = 0, len = 0;
+    while(1){
+        len++;
+        if (fl == 1) bytes_between += 1;
+        if(tmp[i] == 11) fl = 1;
+        if (tmp[i] == 47) break;
+        i++;
+    }
+    if (sizeof(int) == bytes_between+1){
+        return len/sizeof(int);
+    }
+    else if (sizeof(char) == bytes_between+1){
+        return len/sizeof(char);
+    }
+    else if (sizeof(short) == bytes_between+1){
+        return len/sizeof(short);
+    }
+    else if (sizeof(long long) == bytes_between+1){
+        return len/sizeof(long long);
+    }
+    else if (sizeof(unsigned long long) == bytes_between+1){
+        return len/sizeof(unsigned long long);
+    }
+    else if (sizeof(unsigned short) == bytes_between+1){
+        return len/sizeof(unsigned short);
+    }
+
     return 0;
 }
--- 5-pointer/matrix_multy/matrix.h
+++ 5-pointer/matrix_multy/matrix.h
@@ -1,5 +1,15 @@
 #include <math.h>
 
 int* matrix_multy(int* A, int aM, int aN, int* B, int bM, int bN){
+    
+    // for(i = 0; i < m; i++){
+    //     for(j = 0; j < q; j++){
+    //         C[i][j] = 0;
+    //         for(k = 0; k < n; k++){
+    //             C[i][j] += A[i][k] * B[k][j];
+    //         }
+    //     }
+    // }
     return 0;
 }
+
