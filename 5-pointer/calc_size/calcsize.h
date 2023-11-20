32fe1c97f48c31b2c2515cacf31ae5e1dd60feb1
bl4ckk3ys <ilchechnev@gmail.com>
2023-11-20 16:50:07 +0300

    123

--- 5-pointer/calc_size/calcsize.h
+++ 5-pointer/calc_size/calcsize.h
@@ -2,32 +2,14 @@
 
 int calcsize(void* memory){
     char* tmp = memory;
-    int bytes_between = 0, fl = 0, i = 0, ans = 0, len = 0;
+    int bytes_between = 0, fl = 0, i = 0, count;
     while(1){
-        len++;
-        if (fl == 1) bytes_between += 1;
-        if(tmp[i] == 11) fl = 1;
-        if (tmp[i] == 47) break;
+        if(tmp[i] == 11) {
+            fl = 1;
+            count = i;
+        }
+        if (tmp[i] == 47 && fl) return i + (i - count);
         i++;
-    }
-    if (sizeof(int) == bytes_between+1){
-        return len/sizeof(int);
-    }
-    else if (sizeof(char) == bytes_between+1){
-        return len/sizeof(char);
-    }
-    else if (sizeof(short) == bytes_between+1){
-        return len/sizeof(short);
-    }
-    else if (sizeof(long long) == bytes_between+1){
-        return len/sizeof(long long);
-    }
-    else if (sizeof(unsigned long long) == bytes_between+1){
-        return len/sizeof(unsigned long long);
-    }
-    else if (sizeof(unsigned short) == bytes_between+1){
-        return len/sizeof(unsigned short);
-    }
-
+    } 
     return 0;
 }
